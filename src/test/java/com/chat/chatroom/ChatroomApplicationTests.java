99696888d0f68ceedea015ec25395f195001814d
grzesiek-git <nowak.grzegorz140@gmail.com>
2023-07-11 22:53:14 +0200

    init

--- src/test/java/com/chat/chatroom/ChatroomApplicationTests.java
+++ src/test/java/com/chat/chatroom/ChatroomApplicationTests.java
@@ -3,10 +3,8 @@ package com.chat.chatroom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
