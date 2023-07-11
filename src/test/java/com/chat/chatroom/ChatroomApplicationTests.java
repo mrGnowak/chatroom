a268388660763f2efe0910e54187a95c3ff3cc81
Inivo <nowak.grzegorz140@gmail.com>
2023-07-11 14:24:59 +0200

    JUnit config

--- src/test/java/com/chat/chatroom/ChatroomApplicationTests.java
+++ src/test/java/com/chat/chatroom/ChatroomApplicationTests.java
@@ -1,13 +1,22 @@
 package com.chat.chatroom;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ComponentScan(basePackages = "java.com.chat.chatroom")
 class ChatroomApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
+		assertEquals(2, 1 + 1);
 	}
 
 }
